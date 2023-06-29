a50229a4a8adad499a54f7775ca2f8a160364af0
unknown <n.petukhov@innopolis.university>
2023-06-29 12:10:51 +0300

    third

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from "@rollup/plugin-typescript";
+import moment from "@rollup/plugin-commonjs";
 
 export default {
     input: 'src/joke.ts',
     output: { file: 'dist/bundle.js' },
-    plugins: [typescript(), nodeResolve({ browser: true }), terser()],
+    plugins: [moment(), typescript(), nodeResolve({ browser: true }), terser()],
 };
--- src/joke.ts
+++ src/joke.ts
@@ -1,29 +1,43 @@
+import moment from 'moment'
+
 const button = document.getElementById("button-for-api") as HTMLButtonElement
 const titleElement = document.getElementById("title") as HTMLParagraphElement
 const imgElement = document.getElementById("image") as HTMLImageElement
 const dateElement = document.getElementById("date") as HTMLParagraphElement
+const releasedElement = document.getElementById("released") as HTMLParagraphElement
 
 const email: string = 'n.petukhov@innopolis.university'
+
+interface Joke {
+    alt: string,
+    day: string,
+    img: string,
+    month: string,
+    safe_title: string,
+    year: string,
+}
+
+type ID = string
 
 button.addEventListener("click", async () => {
     const params1: URLSearchParams = new URLSearchParams()
     params1.append('email', email)
 
-    const id = await fetch(`https://fwd.innopolis.university/api/hw2?` + params1.toString())
+    const id: ID = await fetch(`https://fwd.innopolis.university/api/hw2?` + params1.toString())
         .then(r => r.json());
-
 
     const params2: URLSearchParams = new URLSearchParams()
     params2.append('id', id)
 
     const res: Response = await fetch('https://fwd.innopolis.university/api/comic?' + params2.toString())
-    const data = await res.json()
+    const data: Joke = await res.json()
 
-    console.log(data)
     const { month, year, day, safe_title, img, alt } = data
 
     titleElement.textContent = safe_title
     dateElement.textContent = "Date: " + day + ":" + month + ":" + year
+
+    releasedElement.textContent = "Released: " + moment().fromNow()
 
     imgElement.src = img
     imgElement.alt = alt
