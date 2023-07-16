fd00cb51e6abe63e72cead1f5c64a423f3184808
unknown <n.petukhov@innopolis.university>
2023-07-16 14:19:27 +0300

    base was fixed

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +9,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		paths: {
-			base: dev ? '' : '/FWD'
-		},
+		// paths: {
+		// 	base: dev ? '' : '/FWD'
+		// },
+
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
