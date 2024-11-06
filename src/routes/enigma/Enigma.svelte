4303afbf57bcc7a6014651ea2e0ac78c5c9ae7b7
Nikolai Petukhov <kolya.petukhov.03@mail.ru>
2024-11-06 16:11:48 +0300

    project page simplification

--- src/routes/enigma/Enigma.svelte
+++ src/routes/enigma/Enigma.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import enigma from '../../images/projects/enigma/enigma2.jpg';
 	// import { base } from '$app/paths';
 </script>
 
@@ -8,9 +7,6 @@
 <div class="project">
 	<div class="container">
 		<h1 class="project-header">Model of Enigma Machine</h1>
-		<div class="project-img">
-			<img src={enigma} alt="" />
-		</div>
 
 		<div class="project-description">
 			<p>Model of Enigma machine in C++ language. It uses German language for interface.</p>
